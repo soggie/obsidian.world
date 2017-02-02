f7db440cf813a53c3b5b692bc2aab5b994679779
Ruben LZ Tan <soggie@gmail.com>
2017-02-02 18:41:43 +0800

    Modified dice to include impact check

--- src/server/dice.js
+++ src/server/dice.js
@@ -2,15 +2,20 @@
 //
 // pos - -1 = dangerous, 0 = balanced, 1 = dominant
 // mod - -1 to +3
+// type - 0 (trait), 1 (edge), 2 (mastery)
 // rand - custom randomisation function, must return a number between 1 and 6
 // returns 0 (miss), 1 (weak hit), 2 (full hit), 3 (crit hit)
-export const dice = (pos, mod, rand) => {
+export const dice = (pos, mod, type, rand) => {
   if (!Number.isInteger(pos) || pos < -1 || pos > 1) {
     return Promise.reject(`pos parameter (actual value: ${pos}) expecting a value of -1, 0 or 1`)
   }
 
   if (!Number.isInteger(mod) || mod < -1 || mod > 3) {
     return Promise.reject(`mod parameter (actual value: ${mod}) expecting a value of -1, 0, 1, 2 or 3`)
+  }
+
+  if (!Number.isInteger(type) || type < 0 || type > 2) {
+    return Promise.reject(`type parameter (actual value: ${type}) expecting a value of 0, 1, or 2`)
   }
 
   if (!rand || typeof rand !== 'function') {
@@ -25,21 +30,18 @@ export const dice = (pos, mod, rand) => {
   ].sort()
 
   let result = 0
-  let impact = 0
+  let impact = roll[type]
   switch (pos) {
     case -1:
       result = roll[0] + roll[1] + mod
-      impact = roll[2]
       break
 
     case 0:
       result = roll[0] + roll[2] + mod
-      impact = roll[1]
       break
 
     case 1:
       result = roll[1] + roll[2] + mod
-      impact = roll[0]
       break
   }
 
--- src/server/test/dice.test.js
+++ src/server/test/dice.test.js
@@ -7,62 +7,62 @@ const stub = (a, b, c) => {
   return () => list.shift()
 }
 
-test('on a roll of [1,2,3], dangerous position should yield a miss (0)', async t => {
-  const result = await dice(-1, 0, stub(1, 2, 3))
+test('roll of [1,2,3], dangerous, trait, should yield a miss (0)', async t => {
+  const result = await dice(-1, 0, 0, stub(1, 2, 3))
   assert.strictEqual(result, 0)
   t.pass()
 })
 
-test('on a roll of [3,4,5], dangerous position should yield a full hit (2)', async t => {
-  const result = await dice(-1, 0, stub(3, 4, 5))
+test('roll of [3,4,5], dangerous, edge, should yield a full hit (2)', async t => {
+  const result = await dice(-1, 0, 1, stub(3, 4, 5))
   assert.strictEqual(result, 2)
   t.pass()
 })
 
-test('on a roll of [3,4,6], dangerous position should yield a crit hit (3)', async t => {
-  const result = await dice(-1, 0, stub(3, 4, 6))
+test('roll of [3,4,6], dangerous, mastery, should yield a crit hit (3)', async t => {
+  const result = await dice(-1, 0, 2, stub(3, 4, 6))
   assert.strictEqual(result, 3)
   t.pass()
 })
 
-test('on a roll of [3,3,3], dangerous position +1 should yield a weak hit (1)', async t => {
-  const result = await dice(-1, 1, stub(3, 3, 3))
+test('roll of [3,3,3], dangerous +1, trait, should yield a weak hit (1)', async t => {
+  const result = await dice(-1, 1, 0, stub(3, 3, 3))
   assert.strictEqual(result, 1)
   t.pass()
 })
 
-test('on a roll of [1,2,3], balanced position should yield a miss (0)', async t => {
-  const result = await dice(0, 0, stub(1, 2, 3))
+test('roll of [1,2,3], balanced, trait, should yield a miss (0)', async t => {
+  const result = await dice(0, 0, 0, stub(1, 2, 3))
   assert.strictEqual(result, 0)
   t.pass()
 })
 
-test('on a roll of [3,3,4], balanced position should yield a weak hit (1)', async t => {
-  const result = await dice(0, 0, stub(3, 3, 4))
+test('roll of [3,3,4], balanced, trait, should yield a weak hit (1)', async t => {
+  const result = await dice(0, 0, 0, stub(3, 3, 4))
   assert.strictEqual(result, 1)
   t.pass()
 })
 
-test('on a roll of [3,4,4], balanced position should yield a full hit (2)', async t => {
-  const result = await dice(0, 0, stub(3, 4, 4))
+test('roll of [3,4,4], balanced, edge, should yield a full hit (2)', async t => {
+  const result = await dice(0, 0, 1, stub(3, 4, 4))
   assert.strictEqual(result, 2)
   t.pass()
 })
 
-test('on a roll of [2,3,4], dominant position should yield a weak hit (1)', async t => {
-  const result = await dice(1, 0, stub(2, 3, 4))
+test('roll of [2,3,4], dominant, edge, should yield a weak hit (1)', async t => {
+  const result = await dice(1, 0, 1, stub(2, 3, 4))
   assert.strictEqual(result, 1)
   t.pass()
 })
 
-test('on a roll of [2,3,3], dominant position +1 should yield a weak hit (1)', async t => {
-  const result = await dice(1, 1, stub(2, 3, 3))
+test('roll of [2,3,3], dominant +1, mastery, should yield a weak hit (1)', async t => {
+  const result = await dice(1, 1, 2, stub(2, 3, 3))
   assert.strictEqual(result, 1)
   t.pass()
 })
 
 test('if no rand provided, it should not throw an error', async t => {
-  const result = await dice(1, 0)
+  const result = await dice(1, 0, 1)
   assert.ok(result >= 0 && result <= 3)
   t.pass()
 })
